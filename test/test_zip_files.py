464e315f20f4b4ea3291091b1c3a5fbe6b1a66d7
Strelnikova Liubov <lubov0strelnikova@gmail.com>
2023-03-18 19:39:35 +0300

    добавила удаление архива, убрала лишнии вызовы текстур, убрала лишние принты

--- test/test_zip_files.py
+++ test/test_zip_files.py
@@ -22,7 +22,8 @@ def create_zip():
             size = os.path.getsize(file_path)
             file_info = f"{name}, {size}"
             file_info_list_source.append(file_info)
-    return file_info_list_source
+    yield file_info_list_source
+    os.remove("../resources/myzip.zip")
 
 
 def test_check_zip(create_zip):
@@ -33,27 +34,22 @@ def test_check_zip(create_zip):
             name = os.path.basename(file.filename)
             file_info = f"{name}, {file.file_size}"
             file_info_list.append(file_info)
-    print(file_info_list)
     assert file_info_list_source == file_info_list
 
 
 def test_check_row_num_in_csv(create_zip):
-    create_zip
     row_count_arc = 0
     with open("../examples/example_csv.csv", "r") as f:
         file = csv.reader(f, delimiter=";")
         row_count = sum(1 for row in file)
-        print(row_count)
     with ZipFile('../resources/myzip.zip') as myzip:
         with myzip.open("example_csv.csv", "r") as myfile:
             for line in myfile:
-                row_count_arc +=1
-            print(row_count_arc)
-    assert row_count == row_count_arc
+                row_count_arc += 1
+    assert row_count == row_count_arc, "Количество строк не совпадает"
 
 
 def test_check_row_num_in_txt(create_zip):
-    create_zip
     with open("../examples/example_txt.txt", "r") as f:
         row_count = sum(1 for row in f)
     with ZipFile('../resources/myzip.zip') as myzip:
@@ -63,11 +59,9 @@ def test_check_row_num_in_txt(create_zip):
 
 
 def test_check_page_num_in_pdf(create_zip):
-    create_zip
     with open("../examples/example_pdf.pdf", "rb") as pdf_file:
         reader = PdfReader(pdf_file)
         count_page = len(reader.pages)
-        print(count_page)
     with ZipFile('../resources/myzip.zip') as myzip:
         with myzip.open("example_pdf.pdf", "r") as myfile:
             reader = PdfReader(myfile)
